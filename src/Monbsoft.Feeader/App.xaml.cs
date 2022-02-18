7d2a37ea98ea0c4419437872ab1f020b41a8c4ee
oliver254 <oliver254@hotmail.fr>
2022-02-18 13:24:54 +0100

    #1 : Create project

---
+++ src/Monbsoft.Feeader/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Monbsoft.Feeader
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
