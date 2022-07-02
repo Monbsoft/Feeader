ed77a53c69d1437ff8d73142064ece16f024f9db
oliver254 <oliver254@hotmail.fr>
2022-07-02 18:59:51 +0200

    #14 : Initial api

---
+++ src/Services/Feeader.SharedKernel/DomainEventDispatcher.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Monbsoft.Feeader.SharedKernel.Interfaces;
+
+namespace Monbsoft.Feeader.SharedKernel;
+
+public class DomainEventDispatcher : IDomainEventDispatcher
+{
+    private readonly IMediator _mediator;
+
+    public DomainEventDispatcher(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public async Task DispatchAndClearEvents(IEnumerable<EntityBase> entitiesWithEvents)
+    {
+        foreach (var entity in entitiesWithEvents)
+        {
+            var events = entity.DomainEvents.ToList();
+            entity.ClearDomainEvents();
+            foreach (var domainEvent in events)
+            {
+                await _mediator.Publish(domainEvent).ConfigureAwait(false);
+            }
+
+        }
+    }
+}
---
+++ src/Services/Feeader.SharedKernel/Interfaces/IDomainEvent.cs
@@ -0,0 +1,5 @@
+﻿namespace Monbsoft.Feeader.SharedKernel.Interfaces;
+
+public interface IDomainEvent
+{
+}
